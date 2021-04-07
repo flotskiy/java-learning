f78f0b258943f8dd9ee285dd69dd57891d0db7f2
Alexander Flotskiy <yattvp@ya.ru>
2021-04-07 13:36:31 +0300

    Employee and Counter added

---
+++ tasks/src/march/Counter.java
@@ -0,0 +1,33 @@
+package march;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Counter {
+    public static void main(String[] args) {
+        String[] array = new String [] {
+                "cat", "mouse", "dog", "crocodile", "tiger", "deer",
+                "cat", "mouse", "wolf", "cat", "dog", "dog", "mouse",
+                "dog", "dog"
+        };
+
+        Map<String, Integer> words = new HashMap<>();
+
+        for (String word : array) {
+            addWord(words, word);
+        }
+
+        System.out.println(words);
+    }
+
+    public static boolean addWord(Map<String, Integer> wordsMap, String word) {
+        if (wordsMap.containsKey(word)) {
+            Integer value = wordsMap.get(word);
+            value++;
+            wordsMap.put(word, value);
+            return false;
+        }
+        wordsMap.put(word, 1);
+        return true;
+    }
+}
---
+++ tasks/src/march/Employee.java
@@ -0,0 +1,85 @@
+package march;
+
+import java.util.*;
+
+public class Employee implements Comparable<Employee> {
+    String name;
+    int salary;
+
+    public Employee(String name, int salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Employee employee) {
+        if (getSalary() > employee.getSalary()) {
+            return -1;
+        }
+        if (getSalary() < employee.getSalary()) {
+            return 1;
+        }
+        return getName().compareTo(employee.getName());
+    }
+
+    public static void main(String[] args) {
+
+        List<Employee> staffList = createStaff();
+
+        System.out.println("-- TreeSet test --");
+        Set<Employee> staffSet = new TreeSet<>(staffList);
+        testPrint(staffSet);
+
+        System.out.println("-- List sorting test --");
+        System.out.println("Before:");
+        testPrint(staffList);
+        System.out.println("After:");
+        sortBySalaryAndAlphabet(staffList);
+        testPrint(staffList);
+    }
+
+    public static List<Employee> createStaff() {
+        List<Employee> staff = new ArrayList<>();
+        staff.add(new Employee("Иванов Иван", 75000));
+        staff.add(new Employee("Петров Петр", 100000));
+        staff.add(new Employee("Сидоров Олег", 99000));
+        staff.add(new Employee("Джавов Василий", 75000));
+        staff.add(new Employee("Краснозвездов Семен", 75000));
+        staff.add(new Employee("Петров Александр", 75000));
+        staff.add(new Employee("Алферов Роман", 109000));
+        staff.add(new Employee("Федоров Андрей", 300009));
+        staff.add(new Employee("Васильев Василий", 14000));
+        staff.add(new Employee("Штрафов Артем", 75000));
+        return staff;
+    }
+
+    public static void sortBySalaryAndAlphabet(List<Employee> staff) {
+        staff.sort(
+                Comparator.comparingInt(Employee::getSalary).reversed()
+                        .thenComparing(Employee::getName)
+        );
+    }
+
+    private static void testPrint(Collection<Employee> list) {
+        for (Employee employee : list) {
+            System.out.println(employee.toString());
+        }
+        System.out.println();
+    }
+}
